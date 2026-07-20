a8c08e8b8d1b3d25fd389bf01f9bbfb1522da2b4
spytaspund <spytaspund@gmail.com>
2026-07-20 17:23:02 +0300

    Tile download + semi-working cache

--- YXMaps/MapVC.swift
+++ YXMaps/MapVC.swift
@@ -13,18 +13,24 @@ import CoreText
 class mapViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     var mapLayer: mapCA!
+    private var isInitialLayoutDone = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         scrollView.delegate = self
         
-        let mapSize = CGSize(width: 256 * 16, height: 256 * 16)
+        let mapSize = CGSize(width: 256, height: 256)
         let tiledFrame = CGRect(origin: .zero, size: mapSize)
         
         mapLayer = mapCA(frame: tiledFrame)
         mapLayer.backgroundColor = .lightGray
         scrollView.addSubview(mapLayer)
         scrollView.contentSize = mapSize
+        
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = pow(2.0, 17.0)
+        scrollView.zoomScale = 1.0
         
         let offsetX = (mapSize.width - scrollView.bounds.width) / 2
         let offsetY = (mapSize.height - scrollView.bounds.height) / 2
@@ -33,8 +39,62 @@ class mapViewController: UIViewController, UIScrollViewDelegate {
         print("yeah im loaded bruv")
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        calculateMinZoom()
+        if !isInitialLayoutDone {
+            isInitialLayoutDone = true
+            gotoCoords(lat: 38, lon: 38, zoom: 17) // dummy coords; replace with gps data
+        }
+    }
+    
+    private func calculateMinZoom() {
+        guard scrollView != nil && scrollView.bounds.width > 0 && scrollView.bounds.height > 0 else { return }
+        
+        let minX = scrollView.bounds.width / 256.0
+        let minY = scrollView.bounds.height / 256.0
+        let minZoom = max(minX, minY)
+        
+        scrollView.minimumZoomScale = minZoom
+        scrollView.zoomScale = max(minZoom, scrollView.zoomScale)
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let offsetX = max((scrollView.bounds.width - scrollView.contentSize.width) * 0.5, 0)
+        let offsetY = max((scrollView.bounds.height - scrollView.contentSize.height) * 0.5, 0)
+        mapLayer.center = CGPoint(x: scrollView.contentSize.width * 0.5 + offsetX, y: scrollView.contentSize.height * 0.5 + offsetY)
+    }
+    
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return mapLayer
+    }
+    
+    func gotoCoords(lat: Double, lon: Double, zoom: Int) {
+        let e = 0.0818191908426
+        
+        let latSin = sin(lat * .pi / 180.0)
+        let eLatSin = e * latSin
+        
+        let p1 = log((1.0 + latSin) / (1.0 - latSin))
+        let p2 = e * log((1.0 + eLatSin) / (1.0 - eLatSin))
+        
+        let x = (lon + 180.0) / 360.0
+        let y = 0.5 - (p1 - p2) / (4.0 * .pi)
+        
+        let targetZoom = pow(2.0, Double(zoom))
+        scrollView.zoomScale = CGFloat(targetZoom)
+        
+        let mapSize = 256.0 * targetZoom
+        
+        let targetX = (x * mapSize) - Double(scrollView.bounds.width / 2.0)
+        let targetY = (y * mapSize) - Double(scrollView.bounds.height / 2.0)
+        let maxOffsetX = CGFloat(mapSize) - scrollView.bounds.width
+        let maxOffsetY = CGFloat(mapSize) - scrollView.bounds.height
+        
+        let safeX = max(0, min(CGFloat(targetX), maxOffsetX))
+        let safeY = max(0, min(CGFloat(targetY), maxOffsetY))
+        scrollView.setContentOffset(CGPoint(x: safeX, y: safeY), animated: false)
     }
 }
 
@@ -60,10 +120,12 @@ class mapCA: UIView {
     
     private func setupTiledLayer() {
         self.screenScale = UIScreen.main.scale
+        self.contentScaleFactor = self.screenScale
         if let tiledLayer = self.layer as? CATiledLayer {
             tiledLayer.tileSize = tileSize
-            tiledLayer.levelsOfDetail = 4
-            tiledLayer.levelsOfDetailBias = 4
+            tiledLayer.contentsScale = self.screenScale
+            tiledLayer.levelsOfDetail = 1
+            tiledLayer.levelsOfDetailBias = 17
         }
     }
     
@@ -76,10 +138,13 @@ class mapCA: UIView {
             
             let scale = context.ctm.a
             let logicalScale = scale / self.screenScale
-            let zoom = Int(round(log2(logicalScale)))
+            let zoom = Int(round(log2(max(1.0, logicalScale))))
+            let maxTiles = 1 << zoom
             
             let x = Int(round(rect.origin.x / rect.size.width))
             let y = Int(round(rect.origin.y / rect.size.height))
+            
+            guard x >= 0 && x < maxTiles && y >= 0 && y < maxTiles else { return }
             let key = cacheKey(x: x, y: y, z: zoom)
             
             cacheLock.lock()
@@ -124,6 +189,7 @@ class mapCA: UIView {
                     }
                 }
             }
+            context.restoreGState()
         }
     }
 }
--- YXMaps/yxapi.swift
+++ YXMaps/yxapi.swift
@@ -9,26 +9,56 @@ import Foundation
 import UIKit
 
 struct yxURL {
-    static func tile(x: Int, y: Int, z: Int, apiKey: String) -> URL {
-        let urlString = "https://core-renderer-tiles.maps.yandex.net/tiles?l=map&x=\(x)&y=\(y)&z=\(z)&scale=1&lang=ru_RU&apiKey=\(apiKey)&l=map"
-        print("givin url: \(urlString)")
+    static func tile(x: Int, y: Int, z: Int) -> URL {
+        let urlString = "https://core-renderer-tiles.maps.yandex.net/tiles?l=map&x=\(x)&y=\(y)&z=\(z)&scale=2.0&lang=ru_RU"
+        return URL(string: urlString)!
+    }
+    
+    static func satTile(x: Int, y: Int, z: Int) -> URL {
+        let urlString = "https://sat03.maps.yandex.net/tiles?l=sat&x=\(x)&y=\(y)&z=\(z)&lang=ru_RU"
+        return URL(string: urlString)!
+    }
+    
+    static func overlayTile(x: Int, y: Int, z: Int) -> URL {
+        let urlString = "https://core-renderer-tiles.maps.yandex.net/tiles?l=skl&x=\(x)&y=\(y)&z=\(z)&scale=1&lang=ru_RU"
         return URL(string: urlString)!
     }
 }
+
+struct yxCache {
+    static func tile(x: Int, y: Int, z: Int) -> String {
+        let cacheDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        return (cacheDir as NSString).appendingPathComponent("yxTiles/\(z)/\(x)/\(y).png")
+    }
+}
+
 class yxapi {
     static let shared = yxapi()
-    let apiKey: String = "" // very bad practice
-    
+    private var fileManager = FileManager.default
     func downloadTile(x: Int, y: Int, z: Int, completion: @escaping (UIImage?) -> Void) {
-        NSURLConnection.sendAsynchronousRequest(URLRequest(url: yxURL.tile(x: x, y: y, z: z, apiKey: self.apiKey)), queue: .main) { response, data, error in
-            let httpResponse = response as? HTTPURLResponse
-            print("REQUESTIN TILE AT \(x), \(y), \(z)")
-            if error == nil, let respCode = httpResponse?.statusCode, respCode == 200, let imgData = data, let image = UIImage(data: imgData) {
-                completion(image)
-            } else {
-                print("OH NO TILE RIP AAAA!!")
-                print("STATUS CODE IZ \(httpResponse?.statusCode ?? 676767)")
-                completion(nil)
+        let cachePath = yxCache.tile(x: x, y: y, z: z)
+        
+        if fileManager.fileExists(atPath: cachePath) {
+            completion(UIImage(contentsOfFile: cachePath))
+        } else {
+            NSURLConnection.sendAsynchronousRequest(URLRequest(url: yxURL.tile(x: x, y: y, z: z)), queue: .main) { response, data, error in
+                let httpResponse = response as? HTTPURLResponse
+                print("REQUESTIN TILE AT \(x), \(y), \(z)")
+                if error == nil, let respCode = httpResponse?.statusCode, respCode == 200, let imgData = data, let image = UIImage(data: imgData) {
+                    completion(image)
+                } else {
+                    print("OH NO TILE RIP AAAA!!")
+                    print("STATUS CODE IZ \(httpResponse?.statusCode ?? 676767)")
+                    completion(nil)
+                }
+            }
+        }
+    }
+    
+    func cacheTile(tile: UIImage, x: Int, y: Int, z: Int) {
+        DispatchQueue.global(priority: .low).async {
+            if let imgData = tile.pngData() {
+                (imgData as NSData).write(to: URL(string: yxCache.tile(x: x, y: y, z: z))!, atomically: true)
             }
         }
     }
